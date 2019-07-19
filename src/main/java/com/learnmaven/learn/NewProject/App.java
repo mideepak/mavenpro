397a5b8d2fe17542c3f5b28eb6f0ebcfe45aab30
mideepak <deepakgec05@gmail.com>
2019-07-19 20:37:11 +0530

    Added my project

---
+++ src/main/java/com/learnmaven/learn/NewProject/App.java
@@ -0,0 +1,14 @@
+package com.learnmaven.learn.NewProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Welcome to maven learning online" );
+    }
+}
